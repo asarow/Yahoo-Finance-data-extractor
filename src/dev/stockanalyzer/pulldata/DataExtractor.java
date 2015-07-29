30fc2200ec69f2240ad4470fd04b49ed5e619a93
Amandeep Sarow <asarow@ucsd.edu>
2015-07-28 22:06:10 -0700

    More javadoc changeS

--- src/dev/stockanalyzer/pulldata/DataExtractor.java
+++ src/dev/stockanalyzer/pulldata/DataExtractor.java
@@ -6,95 +6,130 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+
 import java.net.MalformedURLException;
 import java.net.URL;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-/*
- * Responsible for extracting stock data from an API
+/**
+ * Extracts stock data from the Yahoo! YQL/API and stores the stock data for
+ * use in the model view controller.
+ * 
+ * @author Amandeep Sarow
  */
-
 public class DataExtractor {
-	private final String yahooApi = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22";
-	private final String yahooApiEnd = "%22)&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-	private List<String[]> stockTickers = new ArrayList<String[]>();
-	private static final int dataSize = 6;
-	
-	public DataExtractor() {
-	}
-	
-	public String[] pullTickerData (String ticker) {
-		URL url;
-	    InputStream is = null;
-	    BufferedReader br;
-	    String stockXMLLine;
-	    String stockData[] = null;
-		    try {
-				url = new URL(yahooApi + ticker + yahooApiEnd);
-		        try {
-		        	is = url.openStream();  // throws an IOException
-		        } catch (IOException io) {
-		        	System.out.println("The URL failed to retrieve the stock ticker from the API");
-		        	io.printStackTrace();
-		        	return null;
-		        }
-		        br = new BufferedReader(new InputStreamReader(is));
-	
-			        while ((stockXMLLine = br.readLine()) != null) {
-			        	if (stockXMLLine.contains("LastTradePriceOnly")) {
-			        		stockData = processLine(stockXMLLine, ticker);
-			        		stockTickers.add(stockData);
-			        	}
-			        }
-		    } catch (MalformedURLException mue) { //TODO: Check the necessities of these exceptions
-		         mue.printStackTrace();
-		    } catch (IOException ioe) {
-		         ioe.printStackTrace();
-		    } 
-		    return stockData;
-	}
-	
-	private String[] processLine(String stockLine, String tickerSymbol) {
-		String[] stockData = new String[dataSize];
-		
-		int firstIndex = stockLine.indexOf("<LastTradePriceOnly>") + 20;
-		int lastIndex = stockLine.indexOf("</LastTradePriceOnly>");
-		if (firstIndex == -1 || lastIndex == -1) {
-			System.out.println(tickerSymbol + " is not a valid ticker");
-			stockData[0] ="Invalid ticker";
-			return stockData;		
-		}
-		stockData[1] = stockLine.substring(firstIndex, lastIndex);
-		
-		firstIndex = stockLine.indexOf("<Name>")+6;
-		lastIndex = stockLine.indexOf("</Name>");
-		stockData[0] = stockLine.substring(firstIndex, lastIndex);
-		
-		firstIndex = stockLine.indexOf("<Change>") + 8;
-		lastIndex = stockLine.indexOf("</Change>");
-		stockData[2] = stockLine.substring(firstIndex, lastIndex);
-		
-		firstIndex = stockLine.indexOf("<MarketCapitalization>") + 22;
-		lastIndex = stockLine.indexOf("</MarketCapitalization>");
-		stockData[3] = stockLine.substring(firstIndex, lastIndex);
-		
-		firstIndex = stockLine.indexOf("<EBITDA>") + 8;
-		lastIndex = stockLine.indexOf("</EBITDA>");
-		stockData[4] = stockLine.substring(firstIndex, lastIndex);
-		
-		firstIndex = stockLine.indexOf("<PERatio>") + 9;
-		lastIndex = stockLine.indexOf("</PERatio>");
-		stockData[5] = stockLine.substring(firstIndex, lastIndex);
-		
-		return stockData;
-	}
-	
-	public static int getDataSize() {
-		return dataSize;
-	}
+    private final String yahooApi = "https://query.yahooapis.com/v1/public/
+        yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20
+        (%22";
+    private final String yahooApiEnd = "%22)&diagnostics=true&env=store%3A%
+        2F%2Fdatatables.org%2Falltableswithkeys";
+    private List<String[]> stockTickers = new ArrayList<String[]>();
+    private static final int dataSize = 6;
+    
+    public DataExtractor() {
+    }
 
+    /**
+     * Retrieves stock data for a given stock ticker and stores the data into
+     * an arraylist.
+     *
+     * @param ticker the stock ticker input from the user.
+     * @return the stock information as an arraylist.
+     */
+    public String[] pullTickerData (String ticker) {
+	URL url;
+	InputStream is = null;
+	BufferedReader br;
+	String stockXMLLine;
+	String stockData[] = null;
+
+	try {
+	    // The YQL URL which requires modification for the stock ticker 
+	    url = new URL(yahooApi + ticker + yahooApiEnd);
+	    
+	    try {
+		is = url.openStream();  // throws an IOException
+	    } catch (IOException io) {
+		System.out.println("The URL failed to retrieve the stock 
+                ticker from the API");
+		io.printStackTrace();
+		return null;
+	    }
+	    
+	    br = new BufferedReader(new InputStreamReader(is));
+	    
+	    while ((stockXMLLine = br.readLine()) != null) {
+		if (stockXMLLine.contains("LastTradePriceOnly")) {
+		    /* Each line is processed individually starting with the
+		       last trade price of the stock */
+		    stockData = processLine(stockXMLLine, ticker);
+		    stockTickers.add(stockData);
+		}
+	    }
+	} catch (MalformedURLException mue) { 
+	    //TODO: Check the necessities of these exceptions
+	    mue.printStackTrace();
+	} catch (IOException ioe) {
+	    ioe.printStackTrace();
+	} 
+	return stockData;
+    }
+    
+    /**
+     * Processes each line returned from the YQL (in XML format) and returns
+     * the necessary information as an array.
+     *
+     * @param stockLine the relevant line from the XML file.
+     * @param tickerSymbol the stock ticker input by the user.
+     * @return the stock data as an array.
+     */
+    private String[] processLine(String stockLine, String tickerSymbol) {
+	String[] stockData = new String[dataSize];
+	
+	int firstIndex = stockLine.indexOf("<LastTradePriceOnly>") + 20;
+	int lastIndex = stockLine.indexOf("</LastTradePriceOnly>");
+	if (firstIndex == -1 || lastIndex == -1) {
+	    System.out.println(tickerSymbol + " is not a valid ticker");
+	    stockData[0] ="Invalid ticker";
+	    return stockData;		
+	}
+	stockData[1] = stockLine.substring(firstIndex, lastIndex);
+	
+	firstIndex = stockLine.indexOf("<Name>")+6;
+	lastIndex = stockLine.indexOf("</Name>");
+	stockData[0] = stockLine.substring(firstIndex, lastIndex);
+	
+	firstIndex = stockLine.indexOf("<Change>") + 8;
+	lastIndex = stockLine.indexOf("</Change>");
+	stockData[2] = stockLine.substring(firstIndex, lastIndex);
+	
+	firstIndex = stockLine.indexOf("<MarketCapitalization>") + 22;
+	lastIndex = stockLine.indexOf("</MarketCapitalization>");
+	stockData[3] = stockLine.substring(firstIndex, lastIndex);
+	
+	firstIndex = stockLine.indexOf("<EBITDA>") + 8;
+	lastIndex = stockLine.indexOf("</EBITDA>");
+	stockData[4] = stockLine.substring(firstIndex, lastIndex);
+	
+	firstIndex = stockLine.indexOf("<PERatio>") + 9;
+	lastIndex = stockLine.indexOf("</PERatio>");
+	stockData[5] = stockLine.substring(firstIndex, lastIndex);
+	
+	return stockData;
+    }
+    
+    /**
+     * A getter which returns the current size of the stock data.
+     *
+     * @return the number of stock items as data. 
+     */
+    public static int getDataSize() {
+	return dataSize;
+    }    
 }
--- src/dev/stockanalyzer/pulldata/FinancialsExtractor.java
+++ src/dev/stockanalyzer/pulldata/FinancialsExtractor.java
@@ -10,265 +10,346 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Pulls financial statement information from the Yahoo! YQL/API and stores the
+ * data.
+ *
+ * @author Amandeep Sarow
+ */
 public class FinancialsExtractor {
-	private  List<String[]> incomeStatement = new ArrayList<String[]>();
-	private  List<String[]> balanceSheet = new ArrayList<String[]>();
-	private  List<String[]> cashFlowsStatement = new ArrayList<String[]>();
-	private  String yahooAPIFinancialBegin = "https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20yahoo.finance.";
-	private  String yahooAPIFinancialMid = "%20WHERE%20symbol%3D'";
-	private  String yahooAPIFinancialEnd = "'&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-	private final String bs = "balancesheet", is = "incomestatement", cf = "cashflow";
-	private final String[] urlModifier = {bs, is, cf};
-	private String ticker;
+    private  List<String[]> incomeStatement = new ArrayList<String[]>();
+    private  List<String[]> balanceSheet = new ArrayList<String[]>();
+    private  List<String[]> cashFlowsStatement = new ArrayList<String[]>();
+    private  String yahooAPIFinancialBegin = "https://query.yahooapis.com/v1/
+        public/yql?q=SELECT%20*%20FROM%20yahoo.finance.";
+    private  String yahooAPIFinancialMid = "%20WHERE%20symbol%3D'";
+    private  String yahooAPIFinancialEnd = "'&diagnostics=true&env=store%3A%2F%
+        2Fdatatables.org%2Falltableswithkeys";
+    private final String bs = "balancesheet", is = "incomestatement", 
+	cf = "cashflow";
+    private final String[] urlModifier = {bs, is, cf};
+    private String ticker;
 	
-	public FinancialsExtractor() {
-		
-	}
+    public FinancialsExtractor() {	
+    }
+    
+    /**
+     * Pulls financial statement information from the Yahoo! YQL/API. The URL
+     * used within this method does not return the information, rather it 
+     * returns another URL to the specified financial statement.
+     * 
+     * @param ticker the stock ticker input by the user.
+     */
+    public void pullFinancialData(String ticker) {
+	this.ticker = ticker; //TODO: Change
+	URL url;
+	InputStream istream = null;
+	BufferedReader br;
+	String line;
+	
+	for (int i = 0; i <  urlModifier.length; i++) {
+	    try {
+		url = new URL(yahooAPIFinancialBegin + urlModifier[i] + 
+			      yahooAPIFinancialMid + ticker + 
+			      yahooAPIFinancialEnd);
+		try {
+		    istream = url.openStream();  // throws an IOException
+		} catch (IOException io) {
+		    System.out.println("The URL failed to retrieve the stock 
+                    ticker from the API");
+		    io.printStackTrace();
+		}
 
-	public void pullFinancialData(String ticker) {
-		this.ticker = ticker; //TODO: Change
-		URL url;
-	    InputStream istream = null;
-	    BufferedReader br;
-	    String line;
+		br = new BufferedReader(new InputStreamReader(istream));
+
+		while ((line = br.readLine()) != null) {
+		    /* All three financial statements are pulled from the YQL
+		       /API. */
+		    if(line.contains(urlModifier[i])) {
+			if (urlModifier[i].equals(is)) {
+			    pullIncomeStatement(line);
+			} else if (urlModifier[i].equals(bs)) {
+			    pullBalanceSheet(line);
+			} else if (urlModifier[i].equals(cf)) {
+			    pullCashFlows(line);
+			} else {
+			    System.out.println("Could not find financials");
+			}
+		    }
+		}
+	    } catch (MalformedURLException mue) { 
+		//TODO: Check the necessities of these exceptions
+		mue.printStackTrace();
+	    } catch (IOException ioe) {
+		ioe.printStackTrace();
+	    } 
+	}// end for
+    }
+    
+    /** 
+     * Getter which returns the income statement for a given ticker. 
+     *
+     * @return an array containing the income statement information.
+     */
+    public List<String[]> getIncomeStatement() {
+	return incomeStatement;
+    }
+    
+    /** 
+     * Getter which returns the balance sheet for a given ticker.
+     *
+     * @return an array containing the balance sheet information.
+     */
+    public List<String[]> getBalanceSheet() {
+	return balanceSheet;
+    }
+    
+    /**
+     * Getter which returns the statement of cash flows for a given ticker.
+     *
+     * @return an array containing the statement of cash flows information.
+     */
+    public List<String[]> getCashFlowsStatement() {
+	return cashFlowsStatement;
+    }
+    
+    /* Begin private helper methods */
+
+    /* Private methods are supplied a String which contains the direct URL to
+       the financial statement(s). The statements are returned in HTML format
+       therefore they are scraped and only the necessary data is stored. */
+
+    /**
+     * Pulls the income statement information from the supplied URL and stores
+     * the information as an array.
+     *
+     * @param the string containing the  URL to query for the income statement 
+     *        information.
+     */
+    private void pullIncomeStatement(String urlToPass) {
+	int firstIndex = urlToPass.lastIndexOf("CDATA")+6;
+	int lastIndex = urlToPass.lastIndexOf("]]");
+	// The true URL is stored below.
+	String incomeStatementURL = urlToPass.substring(firstIndex, lastIndex);
+	List<String> isToBuild = new ArrayList<String>();
+	URL url;
+	InputStream istream = null;
+	BufferedReader br;
+	String line;
+	boolean startScraping = false;
+	boolean startIncrement = false;
+	int i = 0;
+	
+	try {
+	    url = new URL(incomeStatementURL);
+	    try {
+		istream = url.openStream();
+	    } catch (IOException e) {
+		System.out.println("Failed to retrieve balance sheet URL");
+		e.printStackTrace();
+	    }
 	    
-	    for (int i = 0; i <  urlModifier.length; i++) {
-		    try {
-				url = new URL(yahooAPIFinancialBegin + urlModifier[i] + yahooAPIFinancialMid + ticker + yahooAPIFinancialEnd);
-		        try {
-		        	istream = url.openStream();  // throws an IOException
-		        } catch (IOException io) {
-		        	System.out.println("The URL failed to retrieve the stock ticker from the API");
-		        	io.printStackTrace();
-		        }
-		        br = new BufferedReader(new InputStreamReader(istream));
-			        while ((line = br.readLine()) != null) {
-			        	if(line.contains(urlModifier[i])) {
-			        		if (urlModifier[i].equals(is)) {
-			        			pullIncomeStatement(line);
-			        		} else if (urlModifier[i].equals(bs)) {
-			        			pullBalanceSheet(line);
-			        		} else if (urlModifier[i].equals(cf)) {
-			        			pullCashFlows(line);
-			        		} else {
-			        			System.out.println("Could not find financials");
-			        		}
-			        	}
-			        }
-		    } catch (MalformedURLException mue) { //TODO: Check the necessities of these exceptions
-		         mue.printStackTrace();
-		    } catch (IOException ioe) {
-		         ioe.printStackTrace();
-		    } 
+	    br = new BufferedReader(new InputStreamReader(istream));
+	    
+	    try {
+		while((line = br.readLine())!= null && i != 17) {
+		    /* Start scraping the HTML at this point */ 
+		    if (line.contains("Period Ending")) 
+			startScraping = true;
 		    
-	    }// end for
-	}
-	
-	public List<String[]> getIncomeStatement() {
-		return incomeStatement;
-	}
-	
-	public List<String[]> getBalanceSheet() {
-		return balanceSheet;
-	}
-	
-	public List<String[]> getCashFlowsStatement() {
-		return cashFlowsStatement;
-	}
-	
-	private void pullIncomeStatement(String urlToPass) {
-		int firstIndex = urlToPass.lastIndexOf("CDATA")+6;
-		int lastIndex = urlToPass.lastIndexOf("]]");
-		String incomeStatementURL = urlToPass.substring(firstIndex, lastIndex);
-		List<String> isToBuild = new ArrayList<String>();
-		URL url;
-		InputStream istream = null;
-		BufferedReader br;
-		String line;
-		boolean startScraping = false;
-		boolean startIncrement = false;
-		int i = 0;
-		
-		try {
-			url = new URL(incomeStatementURL);
-			try {
-				istream = url.openStream();
-			} catch (IOException e) {
-				System.out.println("Failed to retrieve balance sheet URL");
-				e.printStackTrace();
+		    if (line.contains("Net Income Applicable To Common Shares"))
+			startIncrement = true;
+		    
+		    if (startScraping == true) {
+			String returned = scrapeExcessHTML(line);
+			if (returned.length() > 0) {
+			    //  buildSheet() stores the necessary data
+			    buildSheet(returned.split(" "));
 			}
-			
-			br = new BufferedReader(new InputStreamReader(istream));
-			
-			try {
-				while((line = br.readLine())!= null && i != 17) {
-					if (line.contains("Period Ending")) 
-						startScraping = true;
-						
-					if (line.contains("Net Income Applicable To Common Shares"))
-						startIncrement = true;
-					
-					if (startScraping == true) {
-						String returned = scrapeExcessHTML(line);
-						if (returned.length() > 0) {
-							buildSheet(returned.split(" "));
-						}
-					}
-					if (startIncrement == true)
-						i++;
-				}
-			} catch (IOException e) {
-				System.out.println("Failed to read from income statement URL");
-				e.printStackTrace();
-			}
-		} catch (MalformedURLException e) {
-			System.out.println("Invalid income statement URL.");
-			e.printStackTrace();
-			return;
+		    }
+		    
+		    if (startIncrement == true)
+			i++;
 		}
+	    } catch (IOException e) {
+		System.out.println("Failed to read from income statement URL");
+		e.printStackTrace();
+	    }
+	} catch (MalformedURLException e) {
+	    System.out.println("Invalid income statement URL.");
+	    e.printStackTrace();
+	    return;
+	}
+    }
+    
+    /**
+     * Pulls the balance sheet information from the supplied URL and stores
+     * the information as an array.
+     *
+     * @param the string containing the  URL to query for the balance sheet 
+     *        information.
+     */
+    private void pullBalanceSheet(String urlToPass) {
+	int firstIndex = urlToPass.lastIndexOf("CDATA")+6;
+	int lastIndex = urlToPass.lastIndexOf("]]");
+	String balanceSheetURL = urlToPass.substring(firstIndex, lastIndex);
+	URL url;
+	InputStream istream = null;
+	BufferedReader br;
+	String line;
+	boolean startScraping = false;
+	boolean startIncrement = false;
+	int i = 0;
+	
+	try {
+	    url = new URL(balanceSheetURL);
+	    try {
+		istream = url.openStream();
+	    } catch (IOException e) {
+		System.out.println("Failed to retrieve balance sheet URL");
+		e.printStackTrace();
+	    }
+	    
+	    br = new BufferedReader(new InputStreamReader(istream));
+	    
+	    try {
+		while((line = br.readLine())!= null && i != 17) {
+		    if (line.contains("Period Ending")) 
+			startScraping = true;
+		    
+		    if (line.contains("Net Tangible Assets"))
+			startIncrement = true;
+		    
+		    if (startScraping == true) {
+			String returned = scrapeExcessHTML(line);
+			//if (returned.length() > 0)
+			//System.out.println(returned);
+		    }
+		    if (startIncrement == true)
+			i++;
+		}
+	    } catch (IOException e) {
+		System.out.println("Failed to read from balance sheet URL");
+		e.printStackTrace();
+	    }
+	} catch (MalformedURLException e) {
+	    System.out.println("Invalid balance sheet URL.");
+	    e.printStackTrace();
+	    return;
+	}		
+    }
+    
+    /**
+     * Pulls the cash flows statement information from the supplied URL and 
+     * stores the information as an array.
+     *
+     * @param the string containing the  URL to query for the cash flows 
+     * statement information.
+     */
+    private void pullCashFlows(String urlToPass) {
+	int firstIndex = urlToPass.lastIndexOf("CDATA")+6;
+	int lastIndex = urlToPass.lastIndexOf("]]");
+	String cashFlowURL = urlToPass.substring(firstIndex, lastIndex);
+	URL url;
+	InputStream istream = null;
+	BufferedReader br;
+	String line;
+	boolean startScraping = false;
+	boolean startIncrement = false;
+	int i = 0;
+	
+	try {
+	    url = new URL(cashFlowURL);
+	    try {
+		istream = url.openStream();
+	    } catch (IOException e) {
+		System.out.println("Failed to retrieve balance sheet URL");
+		e.printStackTrace();
+	    }
+	    
+	    br = new BufferedReader(new InputStreamReader(istream));
+	    
+	    try {
+		while((line = br.readLine())!= null && i != 17) {
+		    if (line.contains("Period Ending")) 
+			startScraping = true;
+		    
+		    if (line.contains("Change In Cash and Cash Equivalents"))
+			startIncrement = true;
+		    
+		    if (startScraping == true) {
+			
+			//	if (returned.length() > 0)
+			//System.out.println(returned);
+		    }
+		    if (startIncrement == true)
+			i++;
+		}
+	    } catch (IOException e) {
+		System.out.println("Failed to read from cash flows URL");
+		e.printStackTrace();
+	    }
+	} catch (MalformedURLException e) {
+	    System.out.println("Invalid cash flows URL.");
+	    e.printStackTrace();
+	    return;
 	}
 	
-	private void pullBalanceSheet(String urlToPass) {
-		int firstIndex = urlToPass.lastIndexOf("CDATA")+6;
-		int lastIndex = urlToPass.lastIndexOf("]]");
-		String balanceSheetURL = urlToPass.substring(firstIndex, lastIndex);
-		URL url;
-		InputStream istream = null;
-		BufferedReader br;
-		String line;
-		boolean startScraping = false;
-		boolean startIncrement = false;
-		int i = 0;
-		
-		try {
-			url = new URL(balanceSheetURL);
-			try {
-				istream = url.openStream();
-			} catch (IOException e) {
-				System.out.println("Failed to retrieve balance sheet URL");
-				e.printStackTrace();
-			}
-			
-			br = new BufferedReader(new InputStreamReader(istream));
-			
-			try {
-				while((line = br.readLine())!= null && i != 17) {
-					if (line.contains("Period Ending")) 
-						startScraping = true;
-						
-					if (line.contains("Net Tangible Assets"))
-						startIncrement = true;
-					
-					if (startScraping == true) {
-						String returned = scrapeExcessHTML(line);
-						//if (returned.length() > 0)
-							//System.out.println(returned);
-					}
-					if (startIncrement == true)
-						i++;
-				}
-			} catch (IOException e) {
-				System.out.println("Failed to read from balance sheet URL");
-				e.printStackTrace();
-			}
-		} catch (MalformedURLException e) {
-			System.out.println("Invalid balance sheet URL.");
-			e.printStackTrace();
-			return;
-		}		
+    }
+    
+    /**
+     * Builds the financial statements as an array.
+     *
+     * @param line a line of data containing financial information.
+     */
+    private void buildSheet(String[] line) {
+	if (line.length == 0) {
+	    return;
+	} else if (line.length == 1) {
+	    System.out.println(line[0]);
+	    return;
 	}
 	
-	private void pullCashFlows(String urlToPass) {
-		int firstIndex = urlToPass.lastIndexOf("CDATA")+6;
-		int lastIndex = urlToPass.lastIndexOf("]]");
-		String cashFlowURL = urlToPass.substring(firstIndex, lastIndex);
-		URL url;
-		InputStream istream = null;
-		BufferedReader br;
-		String line;
-		boolean startScraping = false;
-		boolean startIncrement = false;
-		int i = 0;
-		
-		try {
-			url = new URL(cashFlowURL);
-			try {
-				istream = url.openStream();
-			} catch (IOException e) {
-				System.out.println("Failed to retrieve balance sheet URL");
-				e.printStackTrace();
-			}
-			
-			br = new BufferedReader(new InputStreamReader(istream));
-			
-			try {
-				while((line = br.readLine())!= null && i != 17) {
-					if (line.contains("Period Ending")) 
-						startScraping = true;
-						
-					if (line.contains("Change In Cash and Cash Equivalents"))
-						startIncrement = true;
-					
-					if (startScraping == true) {
-
-					//	if (returned.length() > 0)
-							//System.out.println(returned);
-					}
-					if (startIncrement == true)
-						i++;
-				}
-			} catch (IOException e) {
-				System.out.println("Failed to read from cash flows URL");
-				e.printStackTrace();
-			}
-		} catch (MalformedURLException e) {
-			System.out.println("Invalid cash flows URL.");
-			e.printStackTrace();
-			return;
-		}
-		
+	String combine = "";
+	
+	for (int i = 0; i < line.length; i++) {
+	    if (line[i].isEmpty())
+		continue;
+ 	    
+	    if (Character.isAlphabetic(line[i].charAt(0))) 
+		combine += line[i] + " ";
+	    
+	    if (Character.isDigit(line[i].charAt(0))) {
+		System.out.println(line[i]);
+	    }
+	    
+	} 
+    }
+    
+    /**
+     * Scrapes each HTML line by removing unnecessary characters and only
+     * returning raw data.
+     *
+     * @param line a line of HTML code to be scraped.
+     * @return raw data extracted from the line of HTML code.
+     */
+    private String scrapeExcessHTML(String line) {
+	String lineToModify = line;
+	while (lineToModify.contains("<") || line.contains(">")) {
+	    int firstIndex = lineToModify.indexOf("<");
+	    int nextIndex = lineToModify.indexOf(">")+1;
+	    if (firstIndex == -1 || nextIndex == -1)
+		break;
+	    
+	    String lineToRemove = lineToModify.substring(firstIndex, nextIndex);
+	    lineToModify = lineToModify.replace(lineToRemove, " ");
+	    lineToModify.trim();
 	}
 	
-	private void buildSheet(String[] line) {
-		if (line.length == 0) {
-			return;
-		} else if (line.length == 1) {
-			System.out.println(line[0]);
-			return;
-		}
-		
-		String combine = "";
-
-		for (int i = 0; i < line.length; i++) {
-			if (line[i].isEmpty())
-				continue;
-			
-			if (Character.isAlphabetic(line[i].charAt(0))) 
-				combine += line[i] + " ";
-
-			if (Character.isDigit(line[i].charAt(0))) {
-				System.out.println(line[i]);
-			}
-			
-		} 
+	while(lineToModify.contains("&nbsp;")) {
+	    lineToModify = lineToModify.replaceAll("&nbsp", "").replaceAll
+		(";", "");
 	}
-	
-	private String scrapeExcessHTML(String line) {
-		String lineToModify = line;
-		while (lineToModify.contains("<") || line.contains(">")) {
-			int firstIndex = lineToModify.indexOf("<");
-			int nextIndex = lineToModify.indexOf(">")+1;
-			if (firstIndex == -1 || nextIndex == -1)
-				break;
-
-			String lineToRemove = lineToModify.substring(firstIndex, nextIndex);
-			lineToModify = lineToModify.replace(lineToRemove, " ");
-			lineToModify.trim();
-		}
-		
-		while(lineToModify.contains("&nbsp;")) {
-			lineToModify = lineToModify.replaceAll("&nbsp", "").replaceAll(";", "");
-		}
-			return lineToModify.trim();
-	}
+	return lineToModify.trim();
+    }
 }
