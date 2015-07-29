ff2cf0ed3750efb286def349ee762b571d793881
Amandeep Sarow <asarow@ucsd.edu>
2015-07-28 21:19:37 -0700

    Added javadoc comments

--- src/dev/stockanalyzer/main/StockMain.java
+++ src/dev/stockanalyzer/main/StockMain.java
@@ -12,45 +12,22 @@ import dev.stockanalyzer.gui.StockGUI;
 import dev.stockanalyzer.pulldata.DataExtractor;
 import dev.stockanalyzer.pulldata.FinancialsExtractor;
 
-public class StockMain {
-	private static List<String[]> stockTickers = new ArrayList<String[]>();
-	
-	public static void retrieveStockDataFromFile() throws IOException {
-		File file = new File("Res/Stock_Data_SP500.txt");
-		LineNumberReader read; 
-		
-		try {
-			read = new LineNumberReader(new FileReader(file));
-		} catch (FileNotFoundException e) {
-			System.out.println("The file containing stock tickers was not read properly.");
-			e.printStackTrace();
-			return;
-		}
-		
-		while(true) {
-			try {
-				String line = read.readLine();
-				if (line == null) 
-					break;
-				stockTickers.add(line.split(" " ));
-			} catch (IOException e) {
-				System.out.println("Failure reading a ticker. Continuing...");
-				e.printStackTrace();
-				continue;
-			}
-		} read.close(); // END WHILE LOOP
-	}
-	
-	public static List<String[]> getStockTickers() {
-		return stockTickers;
-	}
+/**
+ * The main class which creates a graphical user interface object, an object
+ * to retrieve stock data (DataExtractor), and an object to retrieve financial
+ * statement information (FinancialsExtractor). A model view controller object
+ * takes all the objects as parameters.
+ *
+ * @author Amandeep Sarow
+ */
 
-	public static void main(String[] args) {
-		StockGUI gui = new StockGUI();
-		DataExtractor extractor = new DataExtractor();
-		FinancialsExtractor financials = new FinancialsExtractor();
-		
-		StockModelViewController mvc = new StockModelViewController(extractor, gui, financials);
-		
-	}
+public class StockMain {
+    public static void main(String[] args) {
+	StockGUI gui = new StockGUI();
+	DataExtractor extractor = new DataExtractor();
+	FinancialsExtractor financials = new FinancialsExtractor();
+	
+	StockModelViewController mvc = new StockModelViewController
+	    (extractor, gui, financials);	
+    }
 }
--- src/dev/stockanalyzer/main/StockModelViewController.java
+++ src/dev/stockanalyzer/main/StockModelViewController.java
@@ -7,36 +7,44 @@ import dev.stockanalyzer.gui.StockGUI;
 import dev.stockanalyzer.pulldata.DataExtractor;
 import dev.stockanalyzer.pulldata.FinancialsExtractor;
 
+/** 
+ * This class is a model view controller which is responsible for acting as a
+ * mediator between the data extraction classes, and the graphical user 
+ * interface.
+ * 
+ * @author Amandeep Sarow
+ */
 public class StockModelViewController {
-	private DataExtractor extractor;
-	private StockGUI gui;
-	private FinancialsExtractor financials;
+    private DataExtractor extractor;
+    private StockGUI gui;
+    private FinancialsExtractor financials;
 
-	StockModelViewController(DataExtractor extractor, StockGUI gui, FinancialsExtractor financials) {
-		this.extractor = extractor;
-		this.gui = gui;
-		this.financials = financials;
-		
-		this.gui.getStockData(new StockButtonListener());
-		this.gui.getFinancialData(new FinancialDataButtonListener());
-		
-	}
+    StockModelViewController(DataExtractor extractor, StockGUI gui, 
+			     FinancialsExtractor financials) {
+	this.extractor = extractor;
+	this.gui = gui;
+	this.financials = financials;
 	
-	class StockButtonListener implements ActionListener {
-		public void actionPerformed(ActionEvent arg0) {
-			String stockTicker = gui.getTicker();
-			String[] stockData = extractor.pullTickerData(stockTicker);
-			gui.displayStockData(stockData);
-		}
+	this.gui.getStockData(new StockButtonListener());
+	this.gui.getFinancialData(new FinancialDataButtonListener());
+    }
+     
+    class StockButtonListener implements ActionListener {
+	public void actionPerformed(ActionEvent arg0) {
+	    String stockTicker = gui.getTicker();
+	    String[] stockData = extractor.
+		pullTickerData(stockTicker);
+	    gui.displayStockData(stockData);
 	}
-	
-	class FinancialDataButtonListener implements ActionListener {
-		public void actionPerformed(ActionEvent arg0) {
-			String stockTicker = gui.getTicker();
-			financials.pullFinancialData(stockTicker);
-		}
+    }
+    
+    class FinancialDataButtonListener implements ActionListener {
+	public void actionPerformed(ActionEvent arg0) {
+	    String stockTicker = gui.getTicker();
+	    financials.pullFinancialData(stockTicker);
 	}
-	
+    }
+    
 	
 	
 	
