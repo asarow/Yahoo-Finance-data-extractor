c17ffbb72be3b9f1e52ae6d67252386fc47b521a
Amandeep Sarow <asarow@ucsd.edu>
2015-07-31 13:48:46 -0700

    initial GUI support for financial statements

--- src/dev/stockanalyzer/gui/StockGUI.java
+++ src/dev/stockanalyzer/gui/StockGUI.java
@@ -2,14 +2,18 @@ package dev.stockanalyzer.gui;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
-
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.JTabbedPane;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import java.util.*;
 
 import dev.stockanalyzer.pulldata.DataExtractor;
 
@@ -24,15 +28,34 @@ public class StockGUI extends JFrame {
     private JPanel centerPanel = new JPanel(new GridLayout(DataExtractor
 							   .getDataSize(), 1));
     private JPanel southPanel = new JPanel();
+    private JPanel isPanel, bsPanel, cfPanel;
     private JTextField tickerField = new JTextField("AAPL", 4);
+    private JTabbedPane pane;
     private JButton tickerButton, buildFinancialsButton;
     private JLabel[] stockLabels = new JLabel[DataExtractor.getDataSize()*2];
+    private JLabel[] isLabels = new JLabel[150];
+    private JLabel[] bsLabels = new JLabel[200];
+    private JLabel[] cfLabels = new JLabel[150];
+    private boolean activeFrame = false;
+    private JFrame backgroundFrame;
+    private final int bgX = 800, bgY = 300;
+    private final String[] headers = {"Operating Expenses", 
+				      "Income from Continuing Operations",
+				      "Non-recurring Events", "Assets", 
+                                      "Current Assets", "Liabilities",
+                                      "Current Liabilities", "Stockholders'" +
+                                      " Equity", "Operating Activities, Cash" +
+                                      " Flows Provided By or Used In", 
+                                      "Investing Activities, Cash Flows" +
+                                      " Provided By or Used In", "Financing" +
+                                      " Activities, Cash Flows Provided By" +
+                                      " or Used In"};
     
     /** Public constructor for creation of the GUI */
     public StockGUI() {		
 	setTitle("Stock Analyzer");
 	setSize(300,300);
-	setLocationRelativeTo(null);
+	setLocation(500,300);
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	setVisible(true);
 	
@@ -42,6 +65,8 @@ public class StockGUI extends JFrame {
 	addNorthComponents();
 	addCenterComponents();
 	addSouthComponents();
+
+	loadBackgroundFrame();
     }
     
     /** 
@@ -52,7 +77,16 @@ public class StockGUI extends JFrame {
     public String getTicker() {
 	return tickerField.getText();
     }
+
+    public boolean isFrameActive() {
+	return activeFrame;
+    }
     
+    public void closeRunningFrame() {
+	backgroundFrame.dispatchEvent(new WindowEvent(backgroundFrame, 
+				      WindowEvent.WINDOW_CLOSING));
+	activeFrame = false;
+    }
     /** 
      * Displays the stock labels and stock data. This method is called inside
      * the StockModelViewController class.
@@ -66,10 +100,19 @@ public class StockGUI extends JFrame {
 		stockLabels[i].setText(stockData[i/2]);
 	}
     }
-    
-    //TODO: Display financial statement information
-    public void displayFinancials() {
-	
+
+    public void displayFinancialStatementsData
+	(ArrayList<ArrayList<String>> is, ArrayList<ArrayList<String>> bs, 
+	 ArrayList<ArrayList<String>> cf) 
+    {
+	activeFrame = true;
+	backgroundFrame.setVisible(true);
+       	backgroundFrame.setLocation(bgX, bgY);
+
+	displayIncomeStatement(is);
+	displayBalanceSheet(bs);
+	displayCashFlows(cf);
+
     }
     
     /** 
@@ -121,4 +164,95 @@ public class StockGUI extends JFrame {
     private void addSouthComponents() {
 	southPanel.add(new JLabel("Test"));
     }
+
+    private void loadBackgroundFrame() {
+        backgroundFrame = new JFrame("Financial Statements");
+	backgroundFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        backgroundFrame.setSize(1000,600);
+	backgroundFrame.setResizable(false);
+
+	pane = new JTabbedPane();
+
+	backgroundFrame.add(pane);
+
+	isPanel = new JPanel(new GridLayout(0, 5));
+	bsPanel = new JPanel(new GridLayout(0, 5));
+	cfPanel = new JPanel(new GridLayout(0, 5));
+	pane.addTab("Income Statement", null, isPanel);
+	pane.addTab("Balance Sheet", null, bsPanel);
+	pane.addTab("Cash Flows", null, cfPanel);
+
+	/* Initialize labels for each type of statement */
+
+        for (int i = 0; i < isLabels.length; i++) {
+	    isPanel.add(isLabels[i] = new JLabel());
+	}
+
+	for (int i = 0; i < bsLabels.length; i++) {
+	    bsPanel.add(bsLabels[i] = new JLabel());
+	}
+
+	for (int i = 0; i < cfLabels.length; i++) {
+	    cfPanel.add(cfLabels[i] = new JLabel());
+	}
+    }
+
+    private void displayIncomeStatement(ArrayList<ArrayList<String>> is) {
+	int counter = 0;
+	for (int i = 0; i < is.size(); i++) {
+	    for (int j = 0; j < is.get(i).size(); j++) {
+		String label = is.get(i).get(j);
+		for (int k = 0; k < headers.length; k++) {
+		    if (label.trim().equals(headers[k])) {
+			for (int l = 0; l < 4; l++) {
+			    is.get(i).add(j+1, " ");
+			}
+		    }
+		}
+		
+		isLabels[counter].setText(is.get(i).get(j));
+		counter++;
+	    }
+	}
+    }
+
+    private void displayBalanceSheet(ArrayList<ArrayList<String>> bs) {
+	int counter = 0;
+	for (int i = 0; i < bs.size(); i++) {
+	    for (int j = 0; j < bs.get(i).size(); j++) {
+		String label = bs.get(i).get(j);
+		for (int k = 0; k < headers.length; k++) {
+		    if (label.trim().equals(headers[k])) {
+			for (int l = 0; l < 4; l++) {
+			    bs.get(i).add(j+1, " ");
+			}
+		    }
+		}
+		
+		bsLabels[counter].setText(bs.get(i).get(j));
+		counter++;
+	    }
+	}
+    }
+
+    private void displayCashFlows(ArrayList<ArrayList<String>> cf) {
+	int counter = 0;
+	for (int i = 0; i < cf.size(); i++) {
+	    for (int j = 0; j < cf.get(i).size(); j++) {
+		String label = cf.get(i).get(j);
+		for (int k = 0; k < headers.length; k++) {
+		    if (label.trim().equals(headers[k])) {
+			for (int l = 0; l < 4; l++) {
+			    cf.get(i).add(j+1, " ");
+			}
+		    }
+		}
+		
+		cfLabels[counter].setText(cf.get(i).get(j));
+		counter++;
+	    }
+	}
+    }
 }
+
+
